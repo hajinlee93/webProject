7b213c2050a17224a6566a45de6b64868c5ae68c
hajinlee93 <142520205+hajinlee93@users.noreply.github.com>
2023-08-25 22:21:24 +0900

    Add files via upload

---
+++ webProject/src/main/java/test/controller/testController.java
@@ -0,0 +1,14 @@
+package test.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testController {
+
+	@RequestMapping("/test.do")
+	public String testView(HttpServletRequest req) {
+		return "test/test";
+	}
+}
